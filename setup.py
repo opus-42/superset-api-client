17552ae042972e641a34054311729304eb8945b5
Emmanuel Bavoux <emmanuel.bavoux@gmail.com>
2021-04-23 01:04:49 +0200

    Add project URL

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     name="superset-api-client",
     version="0.1.0",
     description=("A simple REST Api Client for Apache-Superset"),
+    url="https://github.com/opus-42/superset-api-client",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=find_packages(),
