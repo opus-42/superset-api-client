a69372d379eb1a9314ee56292000ef4673c8f0fa
nogreintel <noah.greher@intel.com>
2021-12-22 13:51:24 -0800

    adding parameter for cert verification

--- supersetapiclient/client.py
+++ supersetapiclient/client.py
@@ -21,12 +21,14 @@ class SupersetClient:
         username,
         password,
         port=8080,
+        verify=True,
     ):
         self.host = host
         self.base_url = self.join_urls(host, "/api/v1")
         self.username = username
         self._password = password
         self.session = requests.Session()
+        self.verify = verify
 
         # Try authentication and define session
         response = self.session.post(self.login_endpoint, json={
@@ -34,7 +36,7 @@ class SupersetClient:
             "password": self._password,
             "provider": "db",
             "refresh": "true"
-        })
+        }, verify=self.verify)
         response.raise_for_status()
         tokens = response.json()
         self._token = tokens.get("access_token")
@@ -44,7 +46,8 @@ class SupersetClient:
         self._csrf_token = None
         csrf_response = self.session.get(
             self.join_urls(self.base_url, "/security/csrf_token"),
-            headers=self._headers
+            headers=self._headers,
+            verify=self.verify
         )
         csrf_response.raise_for_status() # Check CSRF Token went well
         self._csrf_token = csrf_response.json().get("result")
@@ -57,19 +60,23 @@ class SupersetClient:
         # Bind method
         self.get = partial(
             self.session.get,
-            headers=self._headers
+            headers=self._headers,
+            verify=self.verify
         )
         self.post = partial(
             self.session.post,
-            headers=self._headers
+            headers=self._headers,
+            verify=self.verify
         )
         self.put = partial(
             self.session.put,
-            headers=self._headers
+            headers=self._headers,
+            verify=self.verify
         )
         self.delete = partial(
             self.session.delete,
-            headers=self._headers
+            headers=self._headers,
+            verify=self.verify
         )
 
         # Related Objects
