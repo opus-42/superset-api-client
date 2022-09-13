47eebb44e23a68a3169b5ddb63394e9e70646554
ecederstrand <erik@cederstrand.dk>
2022-09-13 10:18:05 +0200

    Fix bash warning. Increase logging in Superset container

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,7 +3,7 @@
 superset db upgrade
 superset init
 superset fab create-admin --username admin --firstname admin --lastname admin --email admin --password admin
-if [ "${LOAD_EXAMPLES}" -eq 1 ]; then
+if [ "${LOAD_EXAMPLES}" -eq "1" ]; then
   superset load_examples
 fi
-superset run -h 0.0.0.0 -p 8080
+FLASK_ENV=development superset run -h 0.0.0.0 -p 8080
